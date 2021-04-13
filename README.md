e3db15cd37880523ec8bcd97a26a540920db9eec
Davidthesoul <davidthesoul@hotmail.com>
2021-04-12 23:04:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TP4-U5-M4-master
+ 
---
+++ src/components/contador.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+export default function Contador() {
+  const contador = useSelector((state) => state.numero);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <header class="header"> Programación Web FullStack - UTN</header>
+      <h1 class="h1">Contador</h1>
+      <h3 class="h3">{contador}</h3>
+      <>
+        <button class="button" onClick={() => dispatch({ type: 'INCREMENTAR' })}>+</button>
+        <button class="button" onClick={() => dispatch({ type: 'DECREMENTAR' })}>-</button>
+        <button class="button" onClick={() => dispatch({ type: 'RESET' })}>Volver a 0</button>
+      </>
+    </>
+  );
+}
+
